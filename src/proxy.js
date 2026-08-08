73760158e260da2b612b90481da590ce89408ec8
khandker-shathil <shathil.khandker@gmail.com>
2026-08-07 23:00:14 -0400

    fixed bugs(2)

--- src/proxy.js
+++ src/proxy.js
@@ -1,11 +1,10 @@
 import { NextResponse } from 'next/server'
 import { auth } from './app/lib/auth'
-import { headers } from 'next/headers';
  
 export async function proxy(request) {
 
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: request.headers,
   });
 
   if (session){
